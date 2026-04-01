d851cf79631801feb7a6ff450702e3536c2c444a
dani <dani@hyders.dev>
2026-04-01 19:20:09 +0000

    feat(firehose): :technologist: inject global event handlers to all calls (tracking call timings for now)

--- svc/firehose/src/calls/index.ts
+++ svc/firehose/src/calls/index.ts
@@ -1,12 +1,39 @@
-import { IFirehoseService } from "@/proto/gen/ts/v3/firehose/firehose.grpc-server";
+import type { IFirehoseService } from "@/proto/gen/ts/v3/firehose/firehose.grpc-server";
 import { getQMs } from "./qm";
 import { getDevices } from "./device";
 import { PVEProxy } from "./proxy";
 import { getResourcePools } from "./resourcePool";
+import { logLevels, logServices, uniformLog } from "../lib/util";
+import type { UntypedHandleCall } from "@grpc/grpc-js";
 
-export const firehoseService: IFirehoseService = {
+export const injectEvent = (fn: Function): UntypedHandleCall => {
+    return async (...args: any[]) => {
+        /* timings */
+        const start = Date.now();
+        const result = await fn(...args);
+        const end = Date.now();
+
+        uniformLog(logLevels["debug"], logServices["grpc-bus"], `${fn.name} took ${end - start}ms`);
+
+        return result;
+    }
+}
+
+export const injectEvents = (service: IFirehoseService): IFirehoseService => {
+    // iterate over all methods and inject events
+    for (const method in service) {
+        const methodCall = service[method];
+        if (typeof methodCall === "function") {
+            service[method] = injectEvent(methodCall);
+        }
+    }
+
+    return service;
+};
+
+export const firehoseService: IFirehoseService = injectEvents({
     getQMs,
     getResourcePools,
     getDevices,
     pVEProxy: PVEProxy
-}
+});
